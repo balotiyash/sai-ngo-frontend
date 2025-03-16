e21407ba7516a17227bd2ed94112e138cf52e299
Yash Balotiya <balotiyash@gmail.com>
2025-03-16 23:35:06 +0530

    Developed projects page

---
+++ pages/projects/current.php
@@ -0,0 +1,128 @@
+<!-- 
+    File: pages/project/current.php
+    Author: Yash Balotiya
+    Description: This file contains the code for the current project page of the website. It contains the details of the current projects of the NGO.
+    Created on: 16/03/2025
+    Last Modified: 16/03/2025
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Tab Details -->
+    <title>SAI NGO | Current Projects</title>
+
+    <!-- Meta Data -->
+    <?php include '../../shared/metaText.html'; ?>
+
+    <!-- Main Stylesheets -->
+    <!-- <link rel="stylesheet" href="./pastProject.css"> -->
+
+    <style>
+        main {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin-top: 50px;
+        }
+
+        section {
+            display: flex;
+            flex-direction: column;
+            margin-bottom: 50px;
+            width: 75%;
+        }
+
+        section h1 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <?php include '../../shared/navbar/navbar.html'; ?>
+
+    <!-- Contact Us -->
+    <main>
+        <section>
+            <h1>Current Projects</h1>
+
+            <p><b>Awareness project-</b> Street plays, slide shows, Magic shows, pamphlet distribution, poster exhibitions being conducted all over the country. More than 5000 shows already conducted for the prevention of HIV/AIDS, TB, Hepatitis and to diminishing stigma and discrimination related to these diseases.</p>
+
+            <br>
+
+            <p><b>Free mobile Clinic-</b> The health educational program for sex workers and their children for HIV/AIDS prevention and control with providing free health care services including condom distribution. This project is based on the peer educators (DIDI-sister- Community Volunteers). SAI has developed brother –sister relationship with Sex Workers in Mumbai. This project is to the Didis by the DIDIs and for the DIDIs. With the active involvement from community we could bring down the HIV prevalence from 38% to 1.70 % from 2003 to till date.</p>
+
+            <br>
+
+            <p><b>Mobile Clinic –</b> Mobile Clinic provides “Comprehensive Health Care Services to Female Sex Workers & their Children of Ten red light areas of South Mumbai. Despite stringent financial condition we are carrying activities effectively. We are extreme need of financial support & hence appeal to the all generous people & funding agencies to kindly extend the said support. <b>This project was funded by CED- Finland, Solidarite Sida, Foundation de France & Sidaction –France during the period from 2002 to 2017. Now SAI is running this project & we need funding partners.</b></p>
+
+            <br>
+
+            <p><b>Drop In Center–</b> DIC provides psychosocial & health care support to PLHA and linkages to other services. At the centers PLHAs are also given facility for recreation, legal aid, counseling, etc. The poor people living with HIV were identified on basis of economic status and given free medical & nutritional support. Involvement of people living with HIV/AIDS by forming PLHIV support Group named “Ektha” so that they voice out for the rights and people realize that a PLHA has same right as well as the ability to work, interact, and share.  SAI was first amongst NGOs to start Free ART in 2005. <b>It was established in 2003 & funded by Solidarite Sida, Sidaction. Marc Foundation & Foundation de France till 2017. Now SAI is running this project.</b></p>
+
+            <br>
+
+            <p>Despite stringent financial condition we are carrying activities effectively. We are extreme need of financial support & hence appeal to the all generous people & funding agencies to kindly extend the said support.</p>
+
+            <br>
+
+            <p><b>SAI-VIHAAN  PROJECT-</b></p>
+            <p>The specific objectives of Vihaan include:</p>
+            <ol>
+                <li>Early linkages of PLHIV to Care ,Support and treatment services</li>
+                <li>Improved treatment adherence and education for PLHIV</li>
+                <li>Expanded positive prevention activities</li>
+                <li>Improved social protection and wellbeing of PLHIV</li>
+                <li>Strengthened community systems and reduced stigma and discrimination</li>
+            </ol>
+
+            <br>
+
+            <p>Under this project we have providing Care & Support to 15000 PLHIV registered in main 3 Public ART Centres at Viz. J.J. , Nair & KEM Hospitals. In  this project  the  said  people are Personally   contacted  by our  Outreach  workers,  counselor  and  doing  the awareness  task  and motivating  them  for  ICTC  Tests.  ART, periodical check-up in  the Public  Hospitals  also bringing  back those clients  who have  discontinued  the  ART from  the Public  Hospitals. In addition to this the  project  is  also  providing  the  assistance  to  these  people  in  getting  the benefits of  the Govt .Schemes  for them. <b>The project is funded by  NMP+Pune since 2015.</b></p>
+
+            <br>
+
+            <p><b>CANCER AID CELL-</b> From  2013  we have  started  providing  the  financial  help  to  the poor  cancer patients. The said  help is  given to  the poor  and  needy  patients  by  verifying  the  records  such  as the Cost certificate, medical reports  of  the  Hospital, Income proof, KYC norms  documents  i.e. Aadhar, pancard etc. And  after completing  our usual  formalities  we  are extending the  help  to  them. During      the      period   from 2013 to till date we have extended   the financial  help   to  150 Cancer patients. <b>We are very thankful to generous individual donors & our fund raiser activists for this cause.</b></p>
+
+            <br>
+
+            <p><b>HEPATITS  B   PROJECT  ( PAHEL  PROJECT) –</b>   In the project we  are  providing  the free health care services to the poor  female sex workers,  pregnant  women  ,  women  in  child bearing age, their family  members  and    residing  in  A,B,C,D & E wards of  Mumbai for  the prevention and  treatment of Hepatitis  B. Our  Out  Reach  Workers   are personally contacting  them and  educating  them about  the awareness of  the disease  and referring them for the necessary  test  in  the Public Hospitals. After the  testing  those women  who  are found  as Negative  are  vaccinated  and  Positive women are given  the required  treatment in the Public Hospitals. As  of  now  569 women  are  registered with  us  and  they are under our active follow-up.</p>
+
+            <p><b>We  have  started  this  project  from  01–03-2017 with funding  support  of  UNITED WAY  MUMBAI  a renowned  NGO having global network.</b></p>
+
+            <br>
+
+            <p><b>PROJECT  OF MANUFACTURING  OF  ECO-FREINDLY  WOODEN  STOVE–</b></p>
+            <p>We are  manufacturing  of   wood fuel  stoves. We have designed  an  ecofriendly  Wood  fuel  stove  which  is  generating  bare  minimum  smoke  and  emission.  By  using  this  stove  we  can protect  the  actual  users  mainly  women in  India  from  the  diseases  like Cough, TB, Eye –disorder,  throat  infections  etc.  we  have proposed  to  market  these  stoves  in the Rural  sector   and  Tribal  areas  at  the  moderate  and  affordable  rates  to  this  poor   community and  protect  their  health  and  environment  as  well.  Our  generous  funding  partner  viz. C E D-Finland  have  provided  us  PEMS  Machinery  ( U S A Made) free  of  cost  for  the  Emission  testing  of these  stoves.  We  have  conducted  4  market  survey  for  these  stoves  in  the  Rural  and  Tribal  areas  of  Thane  and  Palghar  Districts  nearby  to  Mumbai  and  found  that  the  said  stove  is  very much  appreciated  by  the  Rural  and  Tribal  women  and  inclined  to  use  the  same  for  their daily cooking  purpose.  We  have  aimed  to sell  these  stoves  to  the  said  women Group  at affordable cost  and the  income  so  generated  from  it  shall  be  used for  the  medical , nutritional  support of  the  Key  Population  of  our  abovesaid   Target  population/community.</p>
+
+            <br>
+
+            <p><b>REAR Project- ( Educational support for Children of Sex Workers)-</b></p>
+            <p>This project aims to break the intergenerational cycle of prostitution and ensures that discriminated and marginalised children of commercial sex workers (CSWs) have the opportunities towards a healthy and safe future.</p>
+            <p>Under this project we are doing following activities:</p>
+
+            <ul>
+                <li>Run the after-school education club.</li>
+                <li>Conduct regular tests to assess children’s progress.</li>
+                <li>Provide education (material) support to identified children.</li>
+                <li>Provide after-school meals to ensure children obtain adequate nutrition.</li>
+                <li>Assist children with school curricula.</li>
+                <li>Encourage children to pursue hobbies.</li>
+            </ul>
+
+            <p>Due to these activities we have brought more than 500 children of sex workers in education stream & stopped child prostitution.</p>
+
+            <p><b>We are thankful to Hope for Children UK for funding this project from 2017.</b></p>
+        </section>
+    </main>
+
+    <!-- Footer -->
+    <?php include '../../shared/footer/footer.html'; ?>
+</body>
+
+</html>
---
+++ pages/projects/past.php
@@ -0,0 +1,128 @@
+<!-- 
+    File: pages/projects/past.php
+    Author: Yash Balotiya
+    Description: This file contains the code for the past project page of the website. It contains the details of the past projects of the NGO.
+    Created on: 16/03/2025
+    Last Modified: 16/03/2025
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Tab Details -->
+    <title>SAI NGO | Past Projects</title>
+
+    <!-- Meta Data -->
+    <?php include '../../shared/metaText.html'; ?>
+
+    <!-- Main Stylesheets -->
+    <!-- <link rel="stylesheet" href="./pastProject.css"> -->
+
+    <style>
+        main {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin-top: 50px;
+        }
+
+        section {
+            display: flex;
+            flex-direction: column;
+            margin-bottom: 50px;
+            width: 75%;
+        }
+
+        section h1 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <?php include '../../shared/navbar/navbar.html'; ?>
+
+    <!-- Contact Us -->
+    <main>
+        <section>
+            <h1>Past Projects</h1>
+
+            <p><b>Suraksha-</b> Implemented HIV prevention and treatment of sexually transmitted diseases for 2200 floating/street based sex workers since 2004 to 2016. Recruited staff and peer staff from the community and trained to tackle the HIV prevention & reduce the HIV prevalence. <b>The project was funded by Family Health International through Bill & Melinda Gates Foundation since 2004 to 2012 under Aastha Ptoject & thereafter funded by Mumbai District AIDS Control Society (Mumbai) viz. Suraksha.</b></p>
+
+            <br>
+
+            <p><b>Prevention of Parents to Child Transmission (PPTCT)-</b> Prevention of HIV in newborn of HIV infected mothers by strategic intervention of drugs and support programme. 10 widows living with HIV/AIDS and other PLHAs were part of this project.These peers go door to door and create awareness about HIV/AIDS and prevention of parent to child transmission in in A,B, C, F-South & G- South wards of Municipal Corporation of Greater Mumbai (MCGM). They rapidly surveyed the areas and found pregnant women and motivated them to get themselves registered in government hospitals where they were counseled for ICTC and if found positive the spouse was also motivated to undergo the ICTC. If the mother was found to be positive the Out Reach Worker took full responsibility and created awareness for the need of ART, which was given to them and the child after the delivery. This had helped in rapidly preventing the spread of disease from mother to child. SAI has prevented 310 children from getting HIV infection from their HIV infected pregnant mothers since 2006 to 2015. <b>It was funded by Mumbai District AIDS Control Society & National AIDS Control Organisation (NACO)</b></p>
+
+            <br>
+
+            <p><b>Community Care Centre (CCC)-</b> Played a critical role in providing treatment, care and support to people living with HIV/AIDS (PLHA) in Thane district Under NACP-III this centre was attached to 4 ART centres and ensured that 5000 PLHA were provided:</p>
+
+            <ul>
+                <li>counselling for ART drug adherence, nutrition and prevention</li>
+                <li>treatment of opportunistic infection- in patients care/ OPD</li>
+                <li>referral and outreach services for follow up</li>
+                <li>social support services</li>
+            </ul>
+
+            <p>The centre was mandated to seek better community and family response towards PLHA through family counselling for better treatment outcome, the centre provided families of PLHA counselling on the patients nutritional needs, treatment adherence and psychological support.</p>
+
+            <p>CCC had better linkages and referrals to ICTC, DOTS, PPTCT, LAC, other treatment services and interventions. CCC served as a vital link in providing holistic support to PLHA with district hospitals and provided referral service to PLHA when needed. <b>This project was funded by NACO & Karnataka Health Promotion Trust implemented since 2010 to 2015.</b></p>
+
+            <br>
+
+            <p><b>Mobile ICTC in Thane district–</b> This mobile ICTC/STI clinic was providing ‘services-on-wheels’ to the High risk group which included, Female sex workers, migrants, MSM,IDU in collaboration with other 15 NGOs working with 22500 High risk population in Thane District., who were not particularly inclined to visit the static ICTCs. The advantage of running a Mobile ICTC/STI clinic was that it enabled to reach out to hard-to-reach spots and at the same covering very wide areas. Mobile ICTC also helped these High risk group to overcome the cost and inconvenience of having to travel to centers for HIV counseling and testing and spending a lot of time. The mobile clinic was providing following services:</p>
+
+            <p>A Mobile ICTC Clinic had solved many of the above difficulties faced by High risk group through following ways</p>
+
+            <ul>
+                <li>Availability of ICTC services within the easy accessibility of High risk group.</li>
+                <li>Coverage in collaboration with other NGOs of vast geographical areas in Thane district, where large number of High risk group was able to reach without loss of their pay and their convenience.</li>
+                <li>Carrying out preplanned Mobile ICTC clinic activities at most convenient place and most suitable time for maximum number of High risk group</li>
+                <li>Availability of STI and HIV testing services without unnecessary waiting.</li>
+                <li>Availability of rapid HIV test results immediately, avoiding second visit.</li>
+                <li>Availability of medical support from the doctor for general health issues.</li>
+                <li>Support from the collaborating NGO, wherever required, in getting Care and Support Service and for ART registration at nearby ART Centre.</li>
+                <li>Opportunity for High risk group and their clients to avail ICTC services at a given place at the appointed hour.</li>
+            </ul>
+
+            <p><b>This project was funded by Avert/ Public Health Foundation of India (PHFI) & implemented since 2012 to 2014</b></p>
+
+            <br>
+
+            <p><b>Pediatric AIDS Initiative (PAI)-</b> PAI project was giving support and care to 50 Children Living With HIV/AIDS (CLHA). The treatment for CLHAs for opportunistic infections and ART was given by NACO (National AIDS control organization) – <b>This project was funded by Sevadham Trust & Clinton Foundation & was implemented during the period from 2007 to 2011</b></p>
+
+            <p>SAI’s role in this project was:</p>
+
+            <ul>
+                <li>Diagnosis of HIV infection in infants and children.</li>
+                <li>Enumeration of CD4 count in children.</li>
+                <li>Pre – ART care, counseling and support for those who don’t qualify for ART.</li>
+                <li>Counseling and support for those who require ART.</li>
+                <li>Psycho-social support.</li>
+                <li>Nutritional support.</li>
+                <li>Traveling and logistical help for the children.</li>
+            </ul>
+
+            <br>
+
+            <p><b>Certificate Course in Management of HIV/ AIDS</b></p>
+
+            <p>This was devised to make an impact on the HIV AIDS awareness as well as changing the mind-set of the health professional from imminent medical colleges, public hospitals & private sectors were giving health services to PLHAs. It concentrates on giving up-to-date knowledge and motivating the care-givers toward fulfilling their role in the control of HIV scenario & diminishing stigma & discrimination. 15 experts guest faculties from this filed have delivered the presentation with question answers session.</p>
+
+            <p>We have conducted 24 training workshops & 480 participated had attended the same, consisting of 120 each from Medical, Nursing, Para Medical field & Peer Educators, Community Volunteers & stake Holders. <b>This project was funded by Sidaction- France & was implemented in 2008 & 2009.</b></p>
+
+            <br>
+
+            <p><b>Sanman/ Honor – Sanman project was HIV/AIDS preventive program for homosexual and the transgender community funded by Sidaction & Solidarite Sida _France. It was implemented during the period from 2009 to 2016.</b> The sexually minority is largely discriminated and half of this community vulnerable for HIV. This community has formed its own support group and indulges in activities like awareness, safer sex practices, free distribution of condoms- lubrication, ICTC & ART. We had 3000 key population registered under this project. The peers trained in this project were highly motivated for their cause.</p>
+        </section>
+    </main>
+
+    <!-- Footer -->
+    <?php include '../../shared/footer/footer.html'; ?>
+</body>
+
+</html>
