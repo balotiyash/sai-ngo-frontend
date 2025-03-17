40dd5d37de2ffce7b8f26f8ebb55a59a3658371d
Yash Balotiya <balotiyash@gmail.com>
2025-03-17 12:33:48 +0530

    Developed Apni dunia other projects page

---
+++ pages/other-projects/index.php
@@ -0,0 +1,223 @@
+<!-- 
+    File: pages/apni-duniya/index.php
+    Author: Yash Balotiya
+    Description: This file contains the apni-duniya page of the website.
+    Created on: 17/03/2025
+    Last Modified: 17/03/2025
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Tab Details -->
+    <title>SAI NGO | Other Projects</title>
+
+    <!-- Meta Data -->
+    <?php include '../../shared/metaText.html'; ?>
+
+    <!-- Main Stylesheets -->
+    <style>
+        main {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        section {
+            width: 75%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 50px;
+        }
+
+        .accordion {
+            margin-top: 20px;
+            width: 100%;
+        }
+
+        .accordion-body div {
+            margin-bottom: 12px;
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <?php include '../../shared/navbar/navbar.html'; ?>
+
+    <!-- Contact Us -->
+    <main>
+        <section class="leftContainer">
+            <h1>Other Projects</h1>
+
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Apani Duniya – A Village for Orphans & destitute Women
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <div>
+                                <img src="../../assets/images/House.png" alt="House">
+                            </div>
+
+                            <h3>Apani Duniya – A Village for Orphans & destitute Women</h3>
+                            <p>November 18, 2017 - Admin</p>
+
+                            <p><b>We have purchased a land consisting of 13.5 acres it is surrounded by mountains with full greenery & having continues flow of running water through a revolute at Kuhe Village, District-Thane. We have proposed to set up “ Apani Duniya” ( “Our Universe) of Orphans, destitute Women,  Old aged, tribal people & members of SAI family.</b></p>
+
+                            <p><b>Shortly we will be executing our activities for the setting up the followings:-</b></p>
+
+                            <ul>
+                                <li>Free shelter for Orphans- We have come across the cases where the mothers belonging to sex worker community & parents are dying due to AIDS, Re-marriage of deserted / widowed / divorce women, absence of love and security in the families, Family disputes, Unwanted pregnancy of Trafficking / Sexually exploited girls; these orphan and street children are left without care and support.  Hence to prevent them from involving into anti-social & criminal activities such as drug trafficking, child prostitution, theft etc, therefore to bring into mainstream of the society by providing education & create conducive atmosphere to develop their carrier for better future.  There will be separate shelters for boys & girls each will be of 20 no. aggregating to 40 numbers.</li>
+
+                                <li>Free Shelter for destitute Women – From our experience of two & half decades we have observed that whenever female sex workers develop with AIDS they become unable to earn money & disowned by the brothel authorities & left to die on the roads in a very pathetic condition. Similar situation is arising in some cases of the women whose husband die due to AIDS & disowned by in-laws. Therefore to protect them & to make them economically independent we shall provide them vocational training such as agro farming, horti/floriculture, aquaculture, animal husbandry etc.  These women shall play the role of mothers for orphans as stated above.</li>
+
+                                <li>Shelter for old aged- We wish to set up unique home for old aged who have been disowned by their children or nobody is to look after them or ailing & nobody is there to take care of them.</li>
+                            </ul>
+
+                            <br>
+
+                            <p>There are few people who are living singly & their children away from them will be provided homely atmosphere & taken care of.</p>
+
+                            <ul>
+                                <li>Hospice for treatment of beneficiaries & surrounded tribal community- This will admit those patients who are chronically ill, or those patients with HIV/AIDS or T.B. and Cancer who need hospitalization. They will be given nutrition, and all the medicines as and when required.</li>
+                            </ul>
+
+                            <br>
+
+                            <p>Some patients who are terminally ill and breathing their last is usually turned away from hospitals. These patients can be admitted to the hospice where they can receive medications and extends their life. But those who cannot be saved can at least die peacefully and with dignity.</p>
+
+                            <p>Our land is surrounded by tribal community who are socially & economically backward hence as our social obligation we shall provide the facilities of medication & job opportunities.</p>
+
+                            <ul>
+                                <li>Rehabilitation & Vocational Training Centre- For this the center will provide vocational training in developing skills and entrepreneurship to beneficiaries , so they get an alternative source of livelihood and they become models for others to progress.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            AIDS Project
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <div>
+                                <img src="../../assets/images/Aids-360x300.png" alt="AIDS">
+                            </div>
+
+                            <h3>AIDS Project</h3>
+                            <p>November 18, 2017 - Admin</p>
+
+                            <p>2.10 million people & more suffer from HIV in India-</p>
+                            <p>(Sources- National AIDS Control Organisation (NACO)- India)</p>
+                            <p>But</p>
+                            <p>Unlike other sicknesses & diseases, these unfortunate victims are shunned & ostracized & made to suffer double for something which quite often they got for no fault of their own…</p>
+                            <p>“Services offered by…… is to be changed as “Services provided by Social Activities Integration-SAI” –( however the existing information of services shall continue without any change as per our existing website.</p>
+
+                            <br>
+
+                            <p>The existing Project at Social Activities Integration –SAI is to be replaced as shown under: & the project information for each project shall continue with existing information as per our existing website. For the below project we will incorporate the information afterward.</p>
+
+                            <br>
+
+                            <p>Since 1991 we have been combating AIDS vigorously with involving high risk population by carrying out various project activities as shown herebelow.</p>
+
+                            <br>
+
+                            <p><b><u>Past project-</u></b></p>
+                            <ol>
+                                <li>Suraksha</li>
+                                <li>Prevention of Parents to Child Transmission (PPTCT)</li>
+                                <li>Pediatric AIDS Initiative (PAI)</li>
+                                <li>Community Care Centre (CCC)</li>
+                                <li>Mobile ICTC in Thane district</li>
+                            </ol>
+
+                            <br>
+
+                            <p><b><u>Current Projects-</u></b></p>
+                            <ol>
+                                <li>Awareness project</li>
+                                <li>Didi Project</li>
+                                <li>Free mobile & Static Clinic</li>
+                                <li>Drop In Center</li>
+                                <li>Vihaan- Care & Support Centres</li>
+                                <li>Pahal on Hepatitis B</li>
+                                <li>REAR- Reach for Education and Access to Rights</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Cancer AID Cell
+                        </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <div>
+                                <img src="../../assets/images/cancer-360x300.png" alt="">
+                            </div>
+
+                            <h3>Cancer Aid Cell</h3>
+                            <p>January 1, 2003 - Admin</p>
+
+                            <p>SAI  has  since  2013   taken  additional  responsibility  of  providing  support  to  the  poor  and  needy  cancer Patients  for  their  medical  treatment  over  and  above  its  other  activities. We  have  observed  that  the  number  of  cancer  patients  in the world  are  increasing  every  year  and  the poor  and   needy  patients  are  losing  their  lives  due  to  the  lack  of  Medical ,nutritional  support and also due  to the  unawareness  about  the  disease.  The  mortality  rate of  cancer  in the developed  countries  is   Comparatively  lesser  than  under developing  countries  like  ours.  Hence  initially  we  have  started  providing  financial  help  to  all  such  patients  irrespective of  age for  their  cancer treatment</p>
+
+                            <p>However  we  have  now  focused  our  support  to  the poor  kids  patient  because the mortality  rate of  the  cancer patient  children  in  developing  country  like ours  is  1 death  for every  3 minutes  which  is very  much tragic  and  painful  hence  protection  for  them  is  essential. Further  every  child  has  right  to  have  good  health  and  care  for   happy life  If  we  extend  proper  and  timely  help  to  the  kids  patients  they  will  be  cured  and  live  happy  life  and  contribute  to  the  nation  building  activity. The ultimate  goal  for  caring  cancer  kids is to give  high quality health care services for  their  betterment. Since  2013 we  have  helped  to 133 cancer patients  consisting  of  all  ages and  more  than  50%  of   the  same  were  poor  kids. The  kids  are taking  the  treatment  in reputed   hospitals  of  Mumbai  like  TATA  MEMORIAL, WADIA  etc.</p>
+
+                            <p>Presently  10 kids cancer patients  are  taking  the  treatment  in these  Hospitals  and more  than 25 kids  have cured  from  this  disease during  the last  3 years. Our active efforts  are  on to  bring  more  number  of  deserving   kids  patients    under  our  help for  their better  future  and happy  life.</p>
+
+                            <p><b>Following  Kids  are  under Treatment-</b></p>
+                            <ol>
+                                <li>Shrivardhan  Atugade</li>
+                                <li>Payal  Mahale</li>
+                                <li>Mohd. Shahid  Shaikh</li>
+                                <li>Tushar  Panpatil</li>
+                                <li>Veer  Mahale</li>
+                                <li>Maithile  Parmar</li>
+                                <li>Rihansh Shetty</li>
+                                <li>Divya  Gupta</li>
+                                <li>Laxmikumari  Yadav</li>
+                                <li>Sameeksha  Bhise</li>
+                            </ol>
+
+                            <br>
+
+                            <p><b>Follwing   Kids  Treament  is  completed/ Cured</b></p>
+                            <ol>
+                                <li>Sujan  Moinuddin</li>
+                                <li>Sana  Matin Shaikh</li>
+                                <li>Ayesha  Pathan</li>
+                                <li>Raj  Arun Limje</li>
+                                <li>Alfaiz  Imran Chowkar</li>
+                                <li>Payal Tuna Pradhan</li>
+                                <li>Dewang  Ankush Dabde</li>
+                                <li>Vedansh  Nichkola</li>
+                                <li>Shreya  Lende</li>
+                                <li>Amra  Chandija</li>
+                                <li>Dewang  Dubey</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <!-- Footer -->
+    <?php include '../../shared/footer/footer.html'; ?>
+</body>
+
+</html>
