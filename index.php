e7acf971313d1114efe6e7f69ab548815365d705
Yash Balotiya <balotiyash@gmail.com>
2025-03-23 15:03:41 +0530

    Integrated home page and corrected navigation from home page

--- index.php
+++ index.php
@@ -1,7 +1,153 @@
 <!-- 
-    File: index.php
-    Author: Yash Balotiya
-    Description:
-    Created on:
-    Last Modified:
+    File: pages/home/index.php
+    Author: Atharv Mirgal, Yash Balotiya
+    Description: This file is the home page of the website. It contains a brief description of the website and its features.
+    Created on: 23/03/2025
+    Last Modified: 23/03/2025
 -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Tab Details -->
+    <title>SAI NGO | Home</title>
+
+    <!-- Meta Data -->
+    <?php include './shared/metaText.html'; ?>
+
+    <!-- Main Stylesheets -->
+    <link rel="stylesheet" href="./style.css">
+
+    <!-- Scripts -->
+    <script src="./main.js"></script>
+</html>
+
+<body>
+    <!-- Navbar -->
+    <?php include './shared/navbar/navbar.html'; ?>
+
+    <!-- Main Content -->
+
+    <!-- Hero Image -->
+    <div class="hero-image">
+        <div class="hero-text">
+            <h1>SAI NGO</h1>
+            <p>Our Moto - Human Care</p>
+        </div>
+    </div>
+
+    <!-- Mission Section -->
+    <div class="mission-section">
+        <div class="mission-text">
+            <p id="mission-head">WE HELP POOR & UNDERPRIVILEGED PEOPLE & THEIR CHILDREN TO GET CARE, EDUCATION &
+                TREATMENT</p>
+            <div id="mission-desc">
+                <p>
+                    Since 1991 we are providing health care services to the Poor & neglected Sex Workers,
+                    Poor People Living
+                    with HIV & their Children for the Prevention & Treatment, enlightening about human & legal rights to
+                    fight against injustice. Vocational training has brought radical change in their life given up the
+                    prostitution.
+                </p>
+
+                <p>
+                    The education to the children of sex workers & marginalized people has transformed the life pattern
+                    of
+                    them. Because of education child prostitution, human trafficking has been stopped & the children of
+                    marginalized people have secured better education, employment, personality development & joined the
+                    mainstream of the society. Their future will be brighter & they will live happily with their single
+                    mothers & poor parents. Education will make them responsible citizen of the country & they will
+                    perform
+                    the work of nation building. Hence there is absolute need of funding support from your good selves.
+                </p>
+
+                <p>
+                    Since 2013 we have helped 160 Poor Children infected by Cancer for their treatment & now living
+                    happy
+                    life. By knowing our help to the poor Kid Cancer Patients many other such cases are approaching to
+                    us
+                    for the financial help for their treatment. Deserving cases are considered by us.
+                </p>
+
+                <p>
+                    We also provide help care services for the prevention & treatment of Hepatitis B for poor Patients.
+                </p>
+
+                <p>
+                    Apani Duniya- We have proposed a unique project viz. Apani Duniya at Kuhe, Bhiwandi wherein we have
+                    purchased 13.5 acres of land & proposed to construct Shelters for Orphans, destitute Women,
+                    educational,
+                    hospice center, vocational Center. In view of above we earnestly request you to please donate your
+                    mite
+                    for the noble cause.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <!-- About & Partners Section -->
+    <div class="about-partners">
+        <div class="about-text">
+            <h2>WHO ARE WE?</h2>
+            <p>Social Activities Integration (SAI) is a non-profit organization that strives to prevent the spread of
+                HIV/AIDS & Cancer and also brings relief to patients who need support and care. We also provide
+                education.</p>
+        </div>
+
+        <div class="about-video">
+            <iframe src="https://www.youtube.com/embed/SHl0fsnXZk8" frameborder="0" allowfullscreen></iframe>
+        </div>
+
+        <div class="partners-section">
+            <h3>🔸 OUR PARTNERS</h3>
+            <div class="partners-logos">
+                <img src="./assets/home/partner1.jpg" alt="Partner 1">
+                <img src="./assets/home/partner2.jpg" alt="Partner 2">
+                <img src="./assets/home/partner3.jpg" alt="Partner 3">
+            </div>
+        </div>
+    </div>
+
+    <!-- Projects Section -->
+    <div class="projects-section">
+        <h2>OUR PROJECTS</h2>
+
+        <div class="project-cards">
+            <!-- Project 1 -->
+            <div class="project-card" onclick="redirectTo('./events/index.php')">
+                <img src="./assets/home/project1.png" alt="Apani Duniya">
+                <div class="project-card-content">
+                    <h3>Apani Duniya – A Village for Orphans</h3>
+                    <span class="date">November 18, 2017</span>
+                    <p>We have purchased a land consisting of 13.5 acres</p>
+                </div>
+            </div>
+
+            <!-- Project 2 -->
+            <div class="project-card" onclick="redirectTo('./events/index.php')">
+                <img src="./assets/home/project2.png" alt="AIDS Project">
+                <div class="project-card-content">
+                    <h3>AIDS Project</h3>
+                    <span class="date">November 18, 2017</span>
+                    <p>2.10 million people & more suffer from HIV.</p>
+                </div>
+            </div>
+
+            <!-- Project 3 -->
+            <div class="project-card" onclick="redirectTo('./events/index.php')"> 
+                <img src="./assets/home/project2.png" alt="Cancer Aid Cell">
+                <div class="project-card-content">
+                    <h3>Cancer Aid Cell</h3>
+                    <span class="date">January 1, 2003</span>
+                    <p>SAI has since 2013 taken additional initiatives.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <?php include './shared/footer/footer.html'; ?>
+</body>
+
+</html>
--- main.js
+++ main.js
@@ -1,7 +1,65 @@
 /** 
  * File: main.js
  * Author: Yash Balotiya
- * Description: // TODO
- * Created on: 
- * Last Modified: 
+ * Description: This file is the main file of the project. It is the entry point of the project. It is initlizing the the required logical stuffs on startup.
+ * Created on: 23/03/2025
+ * Last Modified: 23/03/2025
 */
+
+window.onload = () => {
+    // Navbar Initialization for index page
+    document.querySelector('#meta-tab-icon').href = './assets/images/sai-logo.png';
+    document.querySelector('#meta-og-image').href = './assets/images/sai-logo.png';
+
+    // document.querySelector('#navbar-style').href = './shared/navbar/navbar.css';
+    // document.querySelector('#navbar-responsive-style').href = './shared/navbar/responsive.css';
+    // document.querySelector('#footer-style').href = './shared/footer/footer.css';
+
+    document.querySelector('#logoHref').href = './';
+    document.querySelector('#navbar-logo').src = './assets/images/Banner.png';
+
+    document.querySelector('.nav-link1').href = 
+    document.querySelector('.nav-link14').href = './pages/donate';
+    document.querySelector('.nav-link2').href = 
+    document.querySelector('.nav-link15').href =  './';
+    document.querySelector('.nav-link3').href = 
+    document.querySelector('.nav-link16').href = './pages/about';
+    document.querySelector('.nav-link4').href = 
+    document.querySelector('.nav-link17').href = './pages/certificates';
+    document.querySelector('.nav-link5').href = 
+    document.querySelector('.nav-link18').href = './pages/events/index.php?category=Upcoming';
+    document.querySelector('.nav-link6').href = 
+    document.querySelector('.nav-link19').href = './pages/events/index.php?category=Current';
+    document.querySelector('.nav-link7').href = 
+    document.querySelector('.nav-link20').href = './pages/events/index.php?category=Past';
+    document.querySelector('.nav-link8').href = 
+    document.querySelector('.nav-link21').href = './pages/news';
+    document.querySelector('.nav-link9').href = 
+    document.querySelector('.nav-link22').href = './pages/projects/past.php';
+    document.querySelector('.nav-link10').href = 
+    document.querySelector('.nav-link23').href = './pages/projects/current.php';
+    document.querySelector('.nav-link11').href = 
+    document.querySelector('.nav-link24').href = './pages/cancer-aids';
+    document.querySelector('.nav-link12').href = 
+    document.querySelector('.nav-link25').href = './pages/other-projects';
+    document.querySelector('.nav-link13').href = 
+    document.querySelector('.nav-link26').href = './pages/contact';
+
+    // Footer Initialization for index page
+    document.querySelector('.foot-link2').href = './';
+    document.querySelector('.foot-link3').href = './pages/about';
+    document.querySelector('.foot-link4').href = './pages/certificates';
+    document.querySelector('.foot-link5').href = './pages/events/index.php?category=Upcoming';
+    document.querySelector('.foot-link6').href = './pages/events/index.php?category=Current';
+    document.querySelector('.foot-link7').href = './pages/events/index.php?category=Past';
+    document.querySelector('.foot-link8').href = './pages/news';
+    document.querySelector('.foot-link9').href = './pages/projects/past.php';
+    document.querySelector('.foot-link10').href = './pages/projects/current.php';
+    document.querySelector('.foot-link11').href = './pages/cancer-aids';
+    document.querySelector('.foot-link12').href = './pages/other-projects';
+    document.querySelector('.foot-link13').href = './pages/contact';
+}
+
+function redirectTo(url) {
+    window.location.href = url;
+}
